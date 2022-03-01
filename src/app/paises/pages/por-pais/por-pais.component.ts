5d036edb5d129764998fe7cab0c6f5a3477f6f7c
Fernando Nicolas <fersanti2896@gmail.com>
2022-02-28 21:59:52 -0600

    Manejando errores en la respuesta API si no se encuentra un país

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -9,16 +9,20 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent{
   termino: string = '';
+  valError: boolean = false;
 
   /* Inyectamos nuestro servicio */
   constructor(private paisService: PaisService) { }
 
   buscar() {
+    this.valError = false;
     console.log(this.termino);
 
     this.paisService.buscarPais(this.termino)
-        .subscribe(resp => {
+        .subscribe((resp) => {
           console.log(resp);
+        }, (err) => {
+          this.valError = true;
         });
   }
 }
