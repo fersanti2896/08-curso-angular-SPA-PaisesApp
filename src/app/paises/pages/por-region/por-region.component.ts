98db79f317b579a70e6a47e427b55f492d7ce9e0
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-01 23:44:50 -0600

    Cascaron en por-region.component

--- src/app/paises/pages/por-region/por-region.component.ts
+++ src/app/paises/pages/por-region/por-region.component.ts
@@ -3,10 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    .button {
+      margin-right: 5px;
+    }
+  `
   ]
 })
 export class PorRegionComponent implements OnInit {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']; 
 
   constructor() { }
 
